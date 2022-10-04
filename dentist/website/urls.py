aacdfd0041b3ec747adf1878b36a47970eaad744
kisomo <tmuthoka@gmail.com>
2022-10-03 23:53:31 -0400

    changed dirs

---
+++ dentist/website/urls.py
@@ -0,0 +1,22 @@
+
+#from django.contrib import admin
+from django.urls import path
+from . import views 
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    #path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('base.html', views.home, name='home'),
+    path('contact.html', views.contact, name='contact'),
+    path('about.html', views.about, name='about'),
+    path('blog-details.html', views.blog_details, name='blog_details'),
+    path('blog.html', views.blog, name='blog'),
+    path('pricing.html', views.pricing, name='pricing'),
+    path('service.html', views.service, name='service'),
+    path('appointment.html', views.appointment, name='appointment'),
+
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
---
+++ dentist/website/views.py
@@ -0,0 +1,86 @@
+from django.shortcuts import render
+from django.core.mail import send_mail
+
+def home(request):
+    return render(request,"base.html")
+
+def contact(request):
+    if request.method == "POST":
+        message_name = request.POST['message-name']
+        message_email = request.POST['message-email']
+        message = request.POST['message']
+
+        '''
+        #send an email
+        send_mail(
+            'message from' + message_name , # email subject
+            message , # email body
+            message_email , # from email
+            ['tmuthoka@gmail.com','terrence.muthoka@citi.com'], # to emai;
+        )
+        '''
+        return render(request,"contact.html",{'message_name':message_name})
+
+    else:
+        return render(request,"contact.html",{})
+
+
+def about(request):
+    return render(request,"about.html")
+
+def blog_details(request):
+    return render(request,"blog-details.html")
+
+def blog(request):
+    return render(request,"blog.html")
+
+def pricing(request):
+    return render(request,"pricing.html")
+
+def service(request):
+    return render(request,"service.html")
+
+
+def appointment(request):
+    if request.method == "POST":
+        your_name = request.POST['your-name']
+        your_phone = request.POST['your-phone']
+        your_email = request.POST['your-email']
+        your_address = request.POST['your-address']
+        your_schedule = request.POST['your-schedule']
+        your_time = request.POST['your-time']
+        your_message = request.POST['your-message']
+        
+        '''
+        #send an email
+        send_mail(
+            'Hello ' + your_name , # email subject
+            'your appointment is at ' + your_time , # email body
+            'please confirm this message: '+ your_message , # from email
+            ['tmuthoka@gmail.com','terrence.muthoka@citi.com'], # to emai;
+        )
+        '''
+
+        text_file = open("address_test.txt", "w")
+        n = text_file.write(your_address)
+        text_file.close()
+
+        
+
+        return render(request,"appointment.html",{
+            'your_name':your_name, 
+            'your_phone':your_phone,
+            'your_email':your_email,
+            'your_address':your_address,
+            'your_schedule':your_schedule,
+            'your_time':your_time,
+            'your_message':your_message
+            })
+
+    else:
+        return render(request,"base.html",{})
+
+
+
+
+
