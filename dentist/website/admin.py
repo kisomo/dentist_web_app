eb5dc6fffa5d048a55516d2f6cbb6dfb5742bfec
kisomo <tmuthoka@gmail.com>
2022-10-10 00:08:05 -0400

    added models

--- dentist/website/admin.py
+++ dentist/website/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . models import Venue, Customer, Upcomming_apt, Finished_apt, Dentist
 
-# Register your models here.
+admin.site.register(Venue)
+admin.site.register(Customer)
+admin.site.register(Upcomming_apt)
+admin.site.register(Finished_apt)
+admin.site.register(Dentist)
+
+
--- dentist/website/models.py
+++ dentist/website/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
 # Create your models here.
+
+class Customer(models.Model):
+    fname = models.CharField(max_length=200)
+
+class Dentist(models.Model):
+    fname = models.CharField(max_length=200)
+
+class Venue(models.Model):
+    vname = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    zip_code = models.CharField(max_length=200)
+    phone = models.CharField(max_length=200)
+    web = models.URLField()
+    email_address = models.EmailField()
+
+
+class Upcomming_apt(models.Model):
+    doc = models.ForeignKey(Dentist, blank=True, null=True, on_delete=models.CASCADE)
+    venue = models.ForeignKey(Venue, blank=True, null=True, on_delete=models.CASCADE)
+    fname = models.CharField(max_length=200)
+    lname = models.CharField(max_length=200)
+    phone_number = models.CharField(max_length=200)
+    email_address = models.CharField(max_length=200)
+    apt_time = models.DateTimeField()
+    is_done = models.BooleanField(default=False)
+    description = models.TextField(max_length=200)
+
+    def __str__(self):
+        return self.fname 
+
+class Finished_apt(models.Model):
+    fname = models.CharField(max_length=200)
+
+
+
